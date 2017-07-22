81d726c8e3b1bb8f29d995af7fbd5cb9ea31c5f1
Ricardo Vila <ricardovila95@gmail.com>
2017-07-22 10:17:26 -0400

    First Commit

---
+++ Todo/TranslatePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using Xamarin.Forms;
+
+namespace Todo
+{
+    public partial class TranslatePage : ContentPage
+    {
+
+        public TranslatePage()
+        {
+            InitializeComponent();
+           
+        }
+
+        
+    }
+}
